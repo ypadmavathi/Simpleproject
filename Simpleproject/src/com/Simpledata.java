c704150704082d625593c5e880c09cbe12f3ff44
padmavathi.y <padmavathi.y@DS-9121AA344592.HCLT.CORP.HCL.IN>
2017-08-28 12:48:02 +0530

    intiated simple output.

---
+++ Simpleproject/src/com/Simpledata.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Simpledata extends Extradata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Extradata ed=new Extradata();
+		Displaydata();
+		ed.display();
+		
+
+	}
+
+	private static void Displaydata() {
+	   System.out.println("HI");
+	   System.out.println("HELLO");
+		
+	}
+
+}
